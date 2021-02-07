39599d15002e720642fc5e1dc1195a61a5bbc102
supechicken <73414985+supechicken@users.noreply.github.com>
2021-02-07 22:09:01 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
 chrome.runtime.onStartup.addListener(function() {
-  chrome.windows.create({url: "/start.html", type: 'normal', state: 'minimized'})
-})
+    chrome.windows.create({url: "/start.html", type: 'normal', state: 'minimized'})
+});
+chrome.runtime.onInstalled.addListener(function(i) {
+    if (i.reason == 'install') {
+        chrome.tabs.create({url: "/option.html"})
+}})
