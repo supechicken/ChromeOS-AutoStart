4ce7dfcf65b0684e9610e1105a05132101827595
supechicken <supechicken666@gmail.com>
2021-11-29 23:49:38 +0800

    Update onstart.js

--- onstart.js
+++ onstart.js
@@ -1,7 +1,7 @@
-chrome.runtime.onStartup.addListener(function() {
+chrome.runtime.onStartup.addListener( () => {
     chrome.windows.create({url: "/run.html", type: 'popup', state: 'minimized'})
 });
-chrome.runtime.onInstalled.addListener(function(i) {
+chrome.runtime.onInstalled.addListener( (i) => {
     if (i.reason == 'install') {
         chrome.windows.create({url: '/option.html', type: 'popup', height: 221, width: 230})
 }})
