2e810ea5e3b4954400fcedca123e4799ad968239
supechicken <supechicken666@gmail.com>
2021-11-30 14:32:17 +0800

    Update run.js

--- run.js
+++ run.js
@@ -2,30 +2,26 @@
 
 chrome.storage.local.get('debug', (debug) => {
     chrome.storage.local.get('start', (cmd) => {
-        chrome.terminalPrivate.onProcessOutput.addListener(processListener);
-        // open a terminal
-        chrome.terminalPrivate.openTerminalProcess('crosh', (pid) => {
-            if (pid < 0) { alert('Error: cannot open crosh!') }
+        chrome.terminalPrivate.onProcessOutput.addListener( (pid, type, output) => {
+          // print terminal output in console
+          if (output.split("\n").includes("__ext_close__\r") && !debug.debug) {
+            // close terminal process
+            chrome.terminalPrivate.closeTerminalProcess(pid);
 
-            chrome.terminalPrivate.sendInput(pid, 'shell\n');
-            chrome.terminalPrivate.sendInput(pid,
-                // prevent unexpected newline
-                `exec nohup bash <<_CMD_\n${cmd.start}\n_CMD_\n`
-            );
+            // close self after sending commands to terminal
+            chrome.windows.getCurrent( (window) => {
+              chrome.windows.remove(window.id);
+            });
+          }
+          console.log(output);
         });
-        
-        function processListener(pid, type, output) {
-            // print terminal output in console
-            console.log(output);
-        }
 
-        // close self after sending commands to terminal
-        if (!debug.debug) {
-            setTimeout( () => {
-                chrome.windows.getCurrent( (window) => {
-                    chrome.windows.remove(window.id)
-                } ) 
-            }, 1000);
-        }
+        // open a terminal process
+        chrome.terminalPrivate.openTerminalProcess('crosh', (pid) => {
+            if (pid < 0) { alert('Error: cannot open crosh!'); }
+            chrome.terminalPrivate.sendInput(pid, 'shell\n');
+            chrome.terminalPrivate.sendInput(pid, `nohup bash <<_CMD_&\n${cmd.start}\n_CMD_\n`); // prevent unexpected newline
+            chrome.terminalPrivate.sendInput(pid, 'echo "__ext_close__"\n');
+        });
     });
 });
