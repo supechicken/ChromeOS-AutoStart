2119cee5a9b062b106984862f7e3a25dcb27ce71
supechicken <supechicken666@gmail.com>
2022-04-12 15:07:04 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
+// dark mode icon handler
+chrome.runtime.onMessage.addListener( (e) => {
+  if (request.scheme == 'dark') {
+      chrome.browserAction.setIcon({
+        path: { '170': 'icon-dark.png' }
+      });
+  }
+});
+
+// open run.html (used to execute command via terminalPrivate API) at login
 chrome.runtime.onStartup.addListener( () => {
     chrome.windows.create({url: "run.html", type: 'popup', state: 'minimized', height: 200, width: 200})
 });
 
+// prompt user to enter a command after install
 chrome.runtime.onInstalled.addListener( (i) => {
     if (i.reason == 'install') {
         chrome.windows.create({url: 'option.html', type: 'popup', height: 300, width: 310})
