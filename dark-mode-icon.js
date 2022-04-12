9aab92de09e514e767938310164d600de89c1060
supechicken <supechicken666@gmail.com>
2022-04-12 14:57:03 +0800

    Create dark-mode-icon.js

---
+++ dark-mode-icon.js
@@ -0,0 +1,5 @@
+// a content script for setting dark mode icon
+// see https://stackoverflow.com/questions/58880234/toggle-chrome-extension-icon-based-on-light-or-dark-mode-browser
+if ( window.matchMedia('(prefers-color-scheme: dark)').matches ) {
+  chrome.runtime.sendMessage({ scheme: 'dark' })
+}
