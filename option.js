e728897c5dcde614c1a0ddc46dffe14035d1658b
supechicken <supechicken666@gmail.com>
2022-05-30 23:31:33 +0800

    do not use `var`

--- option.js
+++ option.js
@@ -1,14 +1,14 @@
 chrome.storage.local.get('start', data => {
   // print current startup command if set
   if (data.start) {
-    document.getElementById('current_cmd').innerHTML =
-      (`Current startup command: '${data.start}'`)
+    document.getElementById('current_cmd').innerText = `Current startup command: '${data.start}'`
   }
 });
 
 document.getElementById('enter').onclick = () => {
-  var autostart = document.getElementById('new_cmd').value,
-    debug = document.getElementById('debug').checked;
+  const autostart = document.getElementById('new_cmd').value,
+            debug = document.getElementById('debug').checked;
+
   chrome.storage.local.set(
     { 'start': autostart, 'debug': debug },
     () => alert(`Command set: ${autostart}`)
