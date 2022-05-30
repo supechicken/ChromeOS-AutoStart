ec627fc9deaa22fc0bbc225180d2186c71250c8e
supechicken <supechicken666@gmail.com>
2022-05-31 00:37:30 +0800

    Now the command input box supports storing by pressing `Enter`

--- option.js
+++ option.js
@@ -1,25 +1,34 @@
-chrome.storage.local.get('start', data => {
-  // print current startup command if set
-  if (data.start) {
-    document.getElementById('current_cmd').innerText = `Current startup command: '${data.start}'`
-  }
-});
+window.onload = () => {
+  chrome.storage.local.get(['start', 'debug'], localStorage => {
+    // print current startup command if set
+    document.getElementById('current_cmd').innerText = `Current startup command: ${localStorage.start || 'not set'}`;
 
-document.getElementById('enter').onclick = () => {
-  const autostart = document.getElementById('new_cmd').value,
-            debug = document.getElementById('debug').checked;
+    // restore checkbox value
+    document.getElementById('debug').checked = localStorage.debug;
+  });
+}
 
-  chrome.storage.local.set(
-    { 'start': autostart, 'debug': debug },
-    () => alert(`Command set: ${autostart}`)
-  );
-};
+function storeCmdToStorage () {
+  const autostart = document.getElementById('new_cmd').value;
+  chrome.storage.local.set( { start: autostart }, () => alert(`Command set: ${autostart}`) );
+}
+
+document.getElementById('new_cmd').onkeyup = (e) => {
+  if (e.key == 'Enter') storeCmdToStorage();
+}
+
+document.getElementById('enter').onclick = () => storeCmdToStorage();
 
 document.getElementById('clear').onclick = () => {
   // reset settings
-  chrome.storage.local.clear(() => alert('Clear'))
+  chrome.storage.local.clear(() => alert('Clear'));
 }
 
 document.getElementById('test').onclick = () => {
   chrome.windows.create({ url: '/run.html', type: 'popup', height: 200, width: 200 });
-};
+}
+
+document.getElementById('debug').onchange = (e) => {
+  const element = e.currentTarget;
+  chrome.storage.local.set({ debug: element.checked });
+}
