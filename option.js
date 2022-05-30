35f87e923a92a30e8466104ff8390f837a19a8f4
supechicken <supechicken666@gmail.com>
2022-05-30 23:29:02 +0800

    Hide history while executing commands on system shell

--- option.js
+++ option.js
@@ -1,27 +1,25 @@
-chrome.storage.local.get('start', (data) => {
-    // print current startup command if set
-    if (data.start) {
-        document.getElementById('current_cmd').innerHTML = 
-            (`Current startup command: '${data.start}'`)
-    }
+chrome.storage.local.get('start', data => {
+  // print current startup command if set
+  if (data.start) {
+    document.getElementById('current_cmd').innerHTML =
+      (`Current startup command: '${data.start}'`)
+  }
 });
 
-document.getElementById('enter').onclick = function() {
-    var autostart = document.getElementById('new_cmd').value,
-            debug = document.getElementById('debug').checked;
-    chrome.storage.local.set(
-        {'start': autostart, 'debug': debug},
-        () => {
-            alert(`Command set: ${autostart}`)
-        }
-    );
+document.getElementById('enter').onclick = () => {
+  var autostart = document.getElementById('new_cmd').value,
+    debug = document.getElementById('debug').checked;
+  chrome.storage.local.set(
+    { 'start': autostart, 'debug': debug },
+    () => alert(`Command set: ${autostart}`)
+  );
 };
 
-document.getElementById('clear').onclick = function() {
-    // reset settings
-    chrome.storage.local.clear( () => { alert('Clear') } )
+document.getElementById('clear').onclick = () => {
+  // reset settings
+  chrome.storage.local.clear(() => alert('Clear'))
 }
 
-document.getElementById('test').onclick = function() {
-    chrome.windows.create({url: '/run.html', type: 'popup', height: 200, width: 200});
+document.getElementById('test').onclick = () => {
+  chrome.windows.create({ url: '/run.html', type: 'popup', height: 200, width: 200 });
 };
--- run.js
+++ run.js
@@ -1,39 +1,45 @@
 // run.js: execute command set in option page
 
-chrome.storage.local.get('debug', (debug) => {
-    chrome.storage.local.get('start', (cmd) => {
-        chrome.terminalPrivate.onProcessOutput.addListener( (pid, type, data) => {
-          let output
-          // on Chrome OS 100+, the onProcessOutput function will return an ArrayBuffer object instead of a string,
-          // we need to convert it to string if an ArrayBuffer is returned.
-          //
-          // For more info, see https://chromium-review.googlesource.com/c/apps/libapps/+/3470612/
-          if (data instanceof ArrayBuffer) {
-            let dec = new TextDecoder('utf-8')
-            output = dec.decode(data)
-          } else {
-            output = data
-          }
+const magic_word = '\x00__ext_close__\x00',  // a string for telling the extension all commands were run successfully
+         decoder = new TextDecoder('utf-8'); // for decoding ArrayBuffer output on Chrome OS 100+
 
-          // print terminal output in console
-          if (output.match(/[^"]__ext_close__/) && !debug.debug) {
-            // close terminal process
-            chrome.terminalPrivate.closeTerminalProcess(pid);
+chrome.storage.local.get(['debug', 'start'], (debug, cmd) => {
+  chrome.terminalPrivate.onProcessOutput.addListener((pid, type, data) => {
+    // on Chrome OS 100+, the onProcessOutput function will return an ArrayBuffer object instead of a string,
+    // we need to convert it to string if an ArrayBuffer is returned.
+    //
+    // For more info, see https://chromium-review.googlesource.com/c/apps/libapps/+/3470612/
+    const output = (data instanceof ArrayBuffer) ? decoder.decode(data) : data;
 
-            // close self after sending commands to terminal
-            chrome.windows.getCurrent( (window) => {
-              chrome.windows.remove(window.id);
-            });
-          }
-          console.log(output);
-        });
+    // print terminal output in console
+    if ( output.match( new RegExp(magic_word) ) && !debug.debug ) {
+      // close terminal process
+      chrome.terminalPrivate.closeTerminalProcess(pid);
 
-        // open a terminal process
-        chrome.terminalPrivate.openTerminalProcess('crosh', (pid) => {
-            if (pid < 0) { alert('Error: cannot open crosh!'); }
-            chrome.terminalPrivate.sendInput(pid, 'shell\n');
-            chrome.terminalPrivate.sendInput(pid, `nohup bash <<_CMD_&\n${cmd.start}\n_CMD_\n`); // prevent unexpected newline
-            chrome.terminalPrivate.sendInput(pid, 'echo "__ext_close__"\n');
-        });
-    });
+      // close self after sending commands to terminal
+      chrome.windows.getCurrent(window => chrome.windows.remove(window.id));
+    }
+
+    // print process output with all special char escaped (except ${cmd.start} as all output of 
+    // ${cmd.start} are redirected to `/tmp/ChromeOS-AutoStart.log`)
+    console.log('[process output]:', JSON.stringify(output));
+  });
+
+  // open a terminal process, run specific command
+  chrome.terminalPrivate.openTerminalProcess('crosh', pid => {
+    if (pid < 0) alert('Error: cannot open crosh!');
+
+    console.log('[debug]:', `Process ID ${pid} started`);
+
+    chrome.terminalPrivate.sendInput(pid, `
+      shell                                            # enter system shell
+      set +o history                                   # disable history log
+
+      nohup ${cmd.start} > /tmp/ChromeOS-AutoStart.log # run specific command in background, redirect output to a file
+                                                       # (/tmp/ChromeOS-AutoStart.log)
+
+      echo -e "\\x00${magic_word}\\x00"                # tell extension to close this process
+      exit 0                                           # exit system shell
+    `)
+  });
 });
