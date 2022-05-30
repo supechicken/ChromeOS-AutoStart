5b31f12363c66bf5bc9f3ac5fae8748d1a453201
supechicken <supechicken666@gmail.com>
2022-05-30 23:37:27 +0800

    Fix error

--- run.js
+++ run.js
@@ -3,7 +3,10 @@
 const magic_word = '\x00__ext_close__\x00',  // a string for telling the extension all commands were run successfully
          decoder = new TextDecoder('utf-8'); // for decoding ArrayBuffer output on Chrome OS 100+
 
-chrome.storage.local.get(['debug', 'start'], (debug, cmd) => {
+chrome.storage.local.get(['debug', 'start'], (localStorage) => {
+  const debug = localStorage.debug,
+          cmd = localStorage.start;
+
   chrome.terminalPrivate.onProcessOutput.addListener((pid, type, data) => {
     // on Chrome OS 100+, the onProcessOutput function will return an ArrayBuffer object instead of a string,
     // we need to convert it to string if an ArrayBuffer is returned.
@@ -12,7 +15,7 @@ chrome.storage.local.get(['debug', 'start'], (debug, cmd) => {
     const output = (data instanceof ArrayBuffer) ? decoder.decode(data) : data;
 
     // print terminal output in console
-    if ( output.match( new RegExp(magic_word) ) && !debug.debug ) {
+    if ( output.match( new RegExp(magic_word) ) && !debug ) {
       // close terminal process
       chrome.terminalPrivate.closeTerminalProcess(pid);
 
@@ -20,8 +23,8 @@ chrome.storage.local.get(['debug', 'start'], (debug, cmd) => {
       chrome.windows.getCurrent(window => chrome.windows.remove(window.id));
     }
 
-    // print process output with all special char escaped (except ${cmd.start} as all output of 
-    // ${cmd.start} are redirected to `/tmp/ChromeOS-AutoStart.log`)
+    // print process output with all special char escaped (except ${cmd} as all output of 
+    // ${cmd} are redirected to `/tmp/ChromeOS-AutoStart.log`)
     console.log('[process output]:', JSON.stringify(output));
   });
 
@@ -29,13 +32,13 @@ chrome.storage.local.get(['debug', 'start'], (debug, cmd) => {
   chrome.terminalPrivate.openTerminalProcess('crosh', pid => {
     if (pid < 0) alert('Error: cannot open crosh!');
 
-    console.log('[debug]:', `Process ID ${pid} started`);
+    console.log('[debug]:', `Process ${pid} started`);
 
     chrome.terminalPrivate.sendInput(pid, `
       shell                                            # enter system shell
       set +o history                                   # disable history log
 
-      nohup ${cmd.start} > /tmp/ChromeOS-AutoStart.log # run specific command in background, redirect output to a file
+      nohup ${cmd} > /tmp/ChromeOS-AutoStart.log # run specific command in background, redirect output to a file
                                                        # (/tmp/ChromeOS-AutoStart.log)
 
       echo -e "\\x00${magic_word}\\x00"                # tell extension to close this process
